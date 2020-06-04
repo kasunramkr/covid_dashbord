19ac67702e22bc2ea75ca120503c4ef34fd5d3f4
kasunramkr <milindakasun@live.com>
2020-06-04 22:17:42 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CovidApiGetService} from './services/covid-api-get.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'untitled';
+  response: any;
+
+  constructor(private covidGET: CovidApiGetService) {
+  }
+
+  ngOnInit() {
+    this.covidGET.testApiCall().subscribe(
+      (response) => {
+        this.response = response;
+        console.log(this.response);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DateTimeComponent} from './componets/date-time/date-time.component';
+import {CovidApiGetService} from './services/covid-api-get.service';
+import {SrilankaDetailsComponent} from './componets/srilanka-details/srilanka-details.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TodayDetailsComponent} from './componets/today-details/today-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateTimeComponent,
+    SrilankaDetailsComponent,
+    TodayDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [CovidApiGetService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
